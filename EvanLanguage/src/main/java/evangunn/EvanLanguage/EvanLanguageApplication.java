0ebb71edd49511b6f749eef56e0d4644bb4985b3
Evan Gunn <egunn93@gmail.com>
2020-10-28 17:53:45 -0500

    Initial Commit (New Spring Project)

---
+++ EvanLanguage/src/main/java/evangunn/EvanLanguage/EvanLanguageApplication.java
@@ -0,0 +1,13 @@
+package evangunn.EvanLanguage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvanLanguageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvanLanguageApplication.class, args);
+	}
+
+}
---
+++ EvanLanguage/src/test/java/evangunn/EvanLanguage/EvanLanguageApplicationTests.java
@@ -0,0 +1,13 @@
+package evangunn.EvanLanguage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvanLanguageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
